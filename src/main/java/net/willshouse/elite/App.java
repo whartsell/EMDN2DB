de26c263b81b40112ff2fb4c8d8b158552dfdbc0
Will Hartsell <whartsell@homeaway.com>
2014-08-29 21:31:55 -0500

    poc of listener just subscribes to ZMQ and dumps messages out to

--- src/main/java/net/willshouse/elite/App.java
+++ src/main/java/net/willshouse/elite/App.java
@@ -1,5 +1,8 @@
 package net.willshouse.elite;
 
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
 /**
  * Hello world!
  *
@@ -8,6 +11,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Prepare our context and subscriber
+        Context context = ZMQ.context(1);
+        Socket subscriber = context.socket(ZMQ.SUB);
+
+        subscriber.connect("tcp://firehose.elite-market-data.net:9050");
+        subscriber.subscribe("".getBytes());
+        //subscriber.subscribe(new byte[1]);
+        while (!Thread.currentThread ().isInterrupted ()) {
+            // Read envelope with address
+            String data = subscriber.recvStr ();
+            // Read message contents
+            System.out.println(data);
+        }
+        subscriber.close ();
+        context.term ();
     }
 }
